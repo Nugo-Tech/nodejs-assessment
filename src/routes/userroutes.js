d126eaddebc881cc0fb519e134a13ea833b8f2a7
Nithushan-Balasingham <nithushanbalasingham@gmail.com>
2023-09-26 07:26:53 +0530

    refactor

--- src/routes/userroutes.js
+++ src/routes/userroutes.js
@@ -5,13 +5,14 @@ const asyncHandler = require('express-async-handler')
 const {check, validationResult} = require('express-validator')
 
 router.post('/addUser',[
-    check('name', 'Name is inavlid').notEmpty(),
-    check('email', 'Email is inavlid').isEmail().notEmpty(),
-    check('address', 'Address is inavlid').notEmpty(),
-    check('city', 'city is inavlid').notEmpty(),
-    check('country', 'country is inavlid').notEmpty(),
-] ,
-usercontroller.addUser)
+                            check('name', 'Name is inavlid').notEmpty(),
+                            check('email', 'Email is inavlid').isEmail().notEmpty(),
+                            check('address', 'Address is inavlid').notEmpty(),
+                            check('city', 'city is inavlid').notEmpty(),
+                            check('country', 'country is inavlid').notEmpty(),
+                        ] ,
+                            usercontroller.addUser)
+
 router.get('/getUser/:id', usercontroller.getUser)
 router.put('/:id', usercontroller.updateUser)
 router.delete('/delete/:id',usercontroller.deleteUser)
