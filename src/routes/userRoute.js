c0cb4865365aa12401ca2ad87342a496ecd7c1d7
tkdeshan <tkdeshan1103@gmail.com>
2023-09-29 20:56:03 +0530

    Create API for read user by id

--- src/routes/userRoute.js
+++ src/routes/userRoute.js
@@ -18,6 +18,26 @@ router.get('/', async (req, res) => {
   }
 });
 
+// Read user by Id
+router.get('/:Id', async (req, res) => {
+  try {
+    const User = await Users.findOne({ Id: req.params.Id });
 
+    if (!User) {
+      return res.status(404).json({
+        message: 'User not found',
+      });
+    }
+
+    return res.status(200).json({
+      User,
+    });
+  } catch (err) {
+    return res.status(400).json({
+      Failed: 'Server error',
+      error: err,
+    });
+  }
+});
 
 module.exports = router;
