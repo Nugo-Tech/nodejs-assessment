c934f6bc16020e31d443c0e91b4edf3276f2f082
tkdeshan <tkdeshan1103@gmail.com>
2023-09-29 14:38:54 +0530

    Create user model

---
+++ src/models/userModel.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+
+// Define the schema for the 'users' collection
+const userSchema = new mongoose.Schema({
+  _id: {
+    type: Number,
+    required: true,
+    unique: true,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  address: {
+    type: String,
+    required: true,
+  },
+  city: {
+    type: String,
+    required: true,
+  },
+  country: {
+    type: String,
+    required: true,
+  },
+});
+
+module.exports = mongoose.model('users', userSchema);
