877b950b6f32f447867f1fc5f0ea9428ecd4768f
Dilan Asiri <dilanasiri95@gmail.com>
2023-09-30 13:07:47 +0530

    implement the validation and finalize the project

--- src/api/httpUserController.js
+++ src/api/httpUserController.js
@@ -1,12 +1,30 @@
 import express from 'express';
 import {User} from '../dto/user.js'
-import {saveUser,updateUser,deleteUser} from '../repository/dao.js';
+import {saveUser,updateUser,deleteUser,readUser} from '../repository/dao.js';
+import Joi from 'joi';
 
 export const router = express.Router();
+
+// Validation schema for user data
+const userSchema = Joi.object({
+    id: Joi.string().required(),
+    name: Joi.string().min(3).pattern(new RegExp('^[A-Za-z ]+$')).required(),
+    email: Joi.string().pattern(new RegExp('^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}$')).required(),
+    address: Joi.string().min(3).pattern(new RegExp('^[A-Za-z0-9 ]+$')).required(),
+    city: Joi.string().min(3).pattern(new RegExp('^[A-Za-z0-9 ]+$')).required(),
+    country: Joi.string().min(3).pattern(new RegExp('^[A-Za-z0-9 ]+$')).required(),
+});
 
 router.post("/",async(req,res)=>{
     try{
         let userData = req.body;
+
+        // Validate user data
+        const { error } = userSchema.validate(userData);
+        if (error) {
+            res.status(400).json(error.details);
+            return;
+        }
 
         const user = new User(
             userData.id,
@@ -19,9 +37,9 @@ router.post("/",async(req,res)=>{
         await saveUser(user);
         res.sendStatus(201);
     }catch (e){
-        if (e instanceof Joi.ValidationError) {
-            res.status(400).json(e.details);
-        }
+        // if (e instanceof Joi.ValidationError) {
+        //     res.status(400).json(e.details);
+        // }
         res.status(404).send(e);
     }
 });
@@ -39,6 +57,13 @@ router.patch("/:id",async (req,res)=>{
     try{
         let userData = req.body;
 
+        // Validate user data
+        const { error } = userSchema.validate(userData);
+        if (error) {
+            res.status(400).json(error.details);
+            return;
+        }
+
         const user = new User(
             req.params.id,
             userData.name,
@@ -49,9 +74,9 @@ router.patch("/:id",async (req,res)=>{
         await updateUser(user);
         res.sendStatus(201)
     }catch (e){
-        if (e instanceof Joi.ValidationError) {
-            res.status(400).json(e.details);
-        }
+        // if (e instanceof Joi.ValidationError) {
+        //     res.status(400).json(e.details);
+        // }
         res.status(404).send(e);
     }
 });
