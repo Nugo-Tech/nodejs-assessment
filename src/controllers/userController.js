68b12c28762c4a29996967995ad448e493d848a9
minshaf998 <minshaf998@gmail.com>
2023-10-01 00:20:16 +0530

    Check already email have or not error msg return

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,8 +1,18 @@
 import User from "../models/User.js";
 
+// Check email already have or not
+const checkEmailExists = async (value) => {
+  const user = await User.findOne({ where: { email: value } });
+  if (user) return true;
+  else false;
+};
+
 // Create a new user
 export async function createUser(req, res, next) {
   try {
+    if (checkEmailExists(req.body.email))
+      return res.status(500).json({ error: "Email already exists" });
+
     const user = await User.create(req.body);
     res.status(201).json(user);
   } catch (error) {
@@ -26,6 +36,8 @@ export async function readUser(req, res, next) {
 // Update user information by user ID
 export async function updateUser(req, res, next) {
   try {
+    if (checkEmailExists(req.body.email))
+      return res.status(500).json({ error: "Email already exists" });
     const user = await User.findByPk(req.params.userId);
     if (!user) {
       return res.status(404).json({ error: "User not found" });
