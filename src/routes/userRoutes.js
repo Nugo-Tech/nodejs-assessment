2e31f789fb4a128273d9042b322509c6d37afcf6
minshaf998 <minshaf998@gmail.com>
2023-10-01 00:08:19 +0530

    Added validations for user create and update

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -6,12 +6,16 @@ import {
   updateUser,
   deleteUser,
 } from "../controllers/userController.js";
+import {
+  validateCreateUser,
+  validateUpdateUser,
+} from "../middlewares/validationMiddleware.js";
 
 const router = express.Router();
 
-router.post("/", createUser);
+router.post("/", validateCreateUser, createUser);
 router.get("/:userId", readUser);
-router.put("/:userId", updateUser);
+router.put("/:userId", validateUpdateUser, updateUser);
 router.delete("/:userId", deleteUser);
 
 export default router;
